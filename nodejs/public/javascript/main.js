d9f341862366ce7acc30bd0eb4306f880e4e64ba
Myoeng-Jin-An <assyv448@daum.net>
2024-06-10 06:15:08 +0900

    각 메뉴 테두리 추가 및 오른쪽 메뉴 별점 추가, 카카오 api 추가

--- nodejs/public/javascript/main.js
+++ nodejs/public/javascript/main.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var rightMenu = document.querySelector('.right-menu');
     var leftToggleBtn = document.getElementById('left-toggle-btn');
     var rightToggleBtn = document.getElementById('right-toggle-btn');
+    var menuBToggle = document.getElementById('menuB-toggle');
+    var menuBFlyout = document.getElementById('menuB-flyout');
 
     leftToggleBtn.addEventListener('click', function () {
         leftMenu.classList.toggle('collapsed');
@@ -10,5 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     rightToggleBtn.addEventListener('click', function () {
         rightMenu.classList.toggle('collapsed');
+    });
+
+    menuBToggle.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (menuBFlyout.classList.contains('show')) {
+            menuBFlyout.classList.remove('show');
+            setTimeout(function() {
+                menuBFlyout.style.display = 'none';
+            }, 500); // CSS 애니메이션 시간과 동일하게 설정
+        } else {
+            menuBFlyout.style.display = 'block';
+            setTimeout(function() {
+                menuBFlyout.classList.add('show');
+            }, 10); // 약간의 지연을 주어 display 변경 후 애니메이션이 적용
+        }
     });
 });
